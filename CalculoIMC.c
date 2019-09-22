cb3c3e5efb585afe111e029a72a0f940b8cf1b27
Amarildo <amarildop1dev@gmail.com>
2019-09-21 23:11:04 -0300

    Calcula o IMC de uma pessoa

---
+++ CalculoIMC.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<stdlib.h>
+// =============================================================
+// Algoritmo para calcular o Indice de Massa Corporal de uma pessoa
+// =============================================================
+int main(void){
+
+	float massa, altura, IMC;
+
+	system("clear");
+	printf("Qual sua Massa (Kg)? ");
+	scanf("%f", &massa);
+	printf("Qual Altura (m)? ");
+	scanf("%f", &altura);
+	IMC = massa / (altura*altura);
+	printf("IMC: %.2f \n", IMC);
+		if (IMC >= 18.5 && IMC < 25) { 
+			printf("Parabéns! Você está no seu peso ideal!\n\n"); 
+		}else { 
+			printf("Você não está na faixa de peso ideal!\n\n"); 
+		}
+return 0;
+}
