5fbd1fd5868a040734d3ef33665f27beaf3a84d1
Amarildo <amarildop1dev@gmail.com>
2019-09-22 00:19:04 -0300

    Calcula a media das três notas de um aluno.

---
+++ MediaDasTresNotasDeUmAluno.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* ALGORITMO PARA VERIFICAR SE O ALUNO ESTÁ APROVADO, 
+* REPROVADO OU EM RECUPERAÇÃO.
+*/
+int main(void){
+
+	float N1=0, N2=0, N3=0, media=0;
+
+	system("clear");
+	printf("---> SITUAÇÃO ESCOLAR <---\n\n");
+	printf("Modelo das notas: 5.0 | 7.0 | 9.0\n\n");
+	printf("Primeira Nota: ");
+	scanf("%f",&N1);
+	printf("Segunda Nota: ");
+	scanf("%f",&N2);
+	printf("Terceira Nota: ");
+	scanf("%f",&N3);
+	media = (N1+N2+N3) / 3;
+	printf("---------------------------\n");
+	printf("Sua média foi %.2f\n", media);
+		if (media >= 7) { printf("Você está APROVADO!\n"); }
+		else if (media >= 5 && media <7) { printf("Você está em RECUPERAÇÃO\n");}
+		else { printf("Você foi REPROVADO\n"); }
+	printf("---------------------------\n\n");
+
+	return 0;
+}
